32f6e33c4e70480c90d90b8bbcec5554e942cfd3
Symone95-Sourcesense <165904222+Symone95-Sourcesense@users.noreply.github.com>
2025-07-14 11:06:03 +0200

    Update script_with_10_parameters.sh
    Aggiunte descrizioni esercizio 3

--- script_with_10_parameters.sh
+++ script_with_10_parameters.sh
@@ -5,14 +5,16 @@
 MINPARAMS=10
  
 echo
- 
+
+# Per stampare il nome dello script corrente si utilizza $0
 echo "The name of this script is \"$0\"."
 # Adds ./ for current directory
 echo "The name of this script is \"`basename $0`\"."
 # Strips out path name info (see 'basename')
  
 echo
- 
+
+# Per far riferimento ai parametri passati come parametro utilizzeremo la seguente sintassi: $<posizione_parametro>, dal decimo in poi dovremo wrapparlo con {}: ${10}
 if [ -n "$1" ]              # Tested variable is quoted.
 then
  echo "Parameter #1 is $1"  # Need quotes to escape #
@@ -37,8 +39,9 @@ then
 fi
  
 echo "-----------------------------------"
-echo "All the command-line parameters are: "$*""
- 
+echo "All the command-line parameters are: "$*""  # Per stampare tutti i parametri in una volta sola useremo l'asterisco usato come sempre per prendere "tutti" gli elementi
+
+# Qui controlliamo se il numero dei parametri passati è minore(-lt = less than) del numero indicato nella variabile iniziale $MINPARAMS che corrisponde a 10, nel caso stampa quel log
 if [ $# -lt "$MINPARAMS" ]
 then
   echo
