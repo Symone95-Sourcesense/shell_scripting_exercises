49ff0c3843755b63bdf4d53e27c7a37a1404559f
Symone95-Sourcesense <165904222+Symone95-Sourcesense@users.noreply.github.com>
2025-07-14 11:32:57 +0200

    Update esercizio_5_exit_status.sh
    Aggiunti commenti esercizio 5

--- esercizio_5_exit_status.sh
+++ esercizio_5_exit_status.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
- 
+
+# Ogni comando eseguito ha un codice d'uscita, se 0 significa terminato correttamente, altrimenti significa che ha avuto errori durante l'esecuzione
 echo hello
-echo $?    # Exit status 0 returned because command executed successfully.
- 
+# Con $? chiediamo l'exit code dell'ultimo comando eseguito, in questo caso del comando "echo" eseguito prima
+echo $?    # Exit status 0 returned because command executed successfully. Stamperà quindi 0
+
+# Questo comando non viene riconosciuto quindi stamperà un exit code di errore(127) capiamo così che è andato in errore
 lskdf      # Unrecognized command.
 echo $?    # Non-zero exit status returned -- command failed to execute.
  
 echo
- 
+
+# Possiamo customizzare l'exit code usando appunto il comando "exit" e indicare un codice personalizzato
 exit 113   # Will return 113 to shell.
            # To verify this, type "echo $?" after script terminates.
  
