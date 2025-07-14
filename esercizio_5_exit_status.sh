d8b00f2e2c0e70298eaee1ea15aa42a9a1ab814b
Symone95-Sourcesense <165904222+Symone95-Sourcesense@users.noreply.github.com>
2025-07-14 11:34:42 +0200

    Update esercizio_5_exit_status.sh

--- esercizio_5_exit_status.sh
+++ esercizio_5_exit_status.sh
@@ -14,7 +14,9 @@ echo
 # Possiamo customizzare l'exit code usando appunto il comando "exit" e indicare un codice personalizzato
 exit 113   # Will return 113 to shell.
            # To verify this, type "echo $?" after script terminates.
- 
+
+# Dopo l'esecuzione di questo script, se lanciassimo di nuovo 'echo $?' stamperebbe 113 perchè così che abbiamo deciso che debba terminare questo script
+
 #  By convention, an 'exit 0' indicates success,
 #+ while a non-zero exit value means an error or anomalous condition.
 #  See the "Exit Codes With Special Meanings" appendix.
