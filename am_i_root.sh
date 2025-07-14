20242545a9f2c63d2123a5ba7ed8fc316dceafba
Symone95-Sourcesense <165904222+Symone95-Sourcesense@users.noreply.github.com>
2025-07-14 10:54:41 +0200

    Update am_i_root.sh
    Aggiunti commenti esercizio 2

--- am_i_root.sh
+++ am_i_root.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # am-i-root.sh:   Am I root or not?
- 
-ROOT_UID=0   # Root has $UID 0.
- 
+
+# Root has $UID 0.
+ROOT_UID=0         # Di Default l'utente root ha come ID 0
+
+# Confronto il valore della env $UID che corrisponde all'id dell'utente corrente con l'UID di root(0), se vero allora capirà che è root altrimenti no
 if [ "$UID" -eq "$ROOT_UID" ]  # Will the real "root" please stand up?
 then
   echo "You are root."
@@ -16,7 +18,8 @@ exit 0
 # Code below will not execute, because the script already exited.
  
 # An alternate method of getting to the root of matters:
- 
+
+# Qui facciamo lo stesso controllo di prima, ma in base allo username e non all'uid
 ROOTUSER_NAME=root
  
 username=`id -nu`              # Or...   username=`whoami`
